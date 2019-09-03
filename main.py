ccc8717196562b90d7e3642ffc9d4447c4b66025
Abzwigten <abzwingt@gmail.com>
2019-09-03 20:40:08 +0300

    example html

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ for element in data['elements']:
     lon = element['lon']
     lat = element['lat']
     coords.append((lon, lat))
-print(coords)
 d = dict(coords)
-print(d)
 
 map = folium.Map(location=[41.9260, 12.4686], zoom_start = 13,   tiles = "Stamen Toner")
 for k, v in d.items():
